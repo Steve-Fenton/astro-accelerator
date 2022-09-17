d011bc168fb909d509000d9f59f03ffe42e5f176
Steve Fenton <99181436+steve-fenton-octopus@users.noreply.github.com>
2022-09-17 17:43:19 +0100

    Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     site: `http://astro.stevefenton.co.uk`,
+    integrations: [sitemap()],
 });
