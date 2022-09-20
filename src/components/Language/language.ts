b77ab512b987f62af3cb46c28759a06fb7dcc4d6
Steve Fenton <99181436+steve-fenton-octopus@users.noreply.github.com>
2022-09-20 12:25:47 +0100

    Working out issue with resolving article imports

--- src/components/Language/language.ts
+++ src/components/Language/language.ts
@@ -1,5 +1,12 @@
 export function Lang(lang: string) {
-    return (entry: Entry) => entry[lang];
+    const altLang = lang.indexOf('-') > -1
+        ? lang.split('-')[0]
+        : lang;
+
+    return (entry: Entry) => {
+        // Attempt specific lang (en-GB) and fallback to alt lang (en)
+        return entry[lang] ?? entry[altLang];
+    }
 }
 
 export type Entry = {[key: string]: string};
