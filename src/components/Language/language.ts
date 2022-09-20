6456e7ed19f39b5b8fe869d348ab3d97001843d1
Steve Fenton <99181436+steve-fenton-octopus@users.noreply.github.com>
2022-09-20 12:19:58 +0100

    Fix build

--- src/components/Language/language.ts
+++ src/components/Language/language.ts
@@ -1,13 +1,3 @@
-export class Language {
-    constructor(private lang: string) {
-
-    }
-
-    translate(entry: Entry) {
-        return entry[this.lang];
-    }
-}
-
 export function Lang(lang: string) {
     return (entry: Entry) => entry[lang];
 }
