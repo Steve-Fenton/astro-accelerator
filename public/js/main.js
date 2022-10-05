fbd3f1fe819f85772a59e48dfdf84721fcf8a86f
Steve Fenton <github@stevefenton.co.uk>
2022-10-05 14:55:36 +0100

    YT support

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,7 @@ import { setClickableBlocks } from './modules/click-blocks.js';
 import { addStickyNavigation } from './modules/nav-sticky.js';
 import { addMobileNavigation } from './modules/nav-mobile.js';
 import { addIntersectionObserver } from './modules/animation.js';
+import { enhanceYoutubeLinks } from './modules/youtube.js';
 
 const resizedEventName = addResizedEvent();
 
@@ -13,9 +14,12 @@ setClickableBlocks();
 addStickyNavigation('.site-header', '.site-nav', '.site-nav > ul', resizedEventName);
 addMobileNavigation('.navigation-icon', '.site-nav', resizedEventName);
 addIntersectionObserver('.post-list .list-item, main img, main .note, main blockquote');
+enhanceYoutubeLinks();
 
+// Make code blocks focusable, so they can be keyboard scrolled
 qsa('pre.astro-code').forEach((elem) => elem.setAttribute('tabindex', '0'));
 
+// Open external links in a new tab
 qsa('a[href^=http]').forEach((link) => {
     // Open external links in a new tab
     const destination = new URL(link.href);
