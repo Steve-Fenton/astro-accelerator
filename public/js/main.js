29a69a04bf501a97a21c633f5ee35aab7594b742
Steve Fenton <github@stevefenton.co.uk>
2022-10-07 17:11:19 +0100

    Code block support

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,11 @@
 // @ts-check
-
-import { qsa } from './modules/query.js';
 import { addResizedEvent } from './modules/resizing.js';
 import { setClickableBlocks } from './modules/click-blocks.js';
 import { addStickyNavigation } from './modules/nav-sticky.js';
 import { addMobileNavigation } from './modules/nav-mobile.js';
 import { addIntersectionObserver } from './modules/animation.js';
 import { enhanceYoutubeLinks } from './modules/youtube.js';
+import { enhanceCodeBlocks } from './modules/code-blocks.js';
 
 const resizedEventName = addResizedEvent();
 
@@ -15,15 +14,4 @@ addStickyNavigation('.site-header', '.site-nav', '.site-nav > ul', resizedEventN
 addMobileNavigation('.navigation-icon', '.site-nav', resizedEventName);
 addIntersectionObserver('.post-list .list-item, main img, main .note, main blockquote');
 enhanceYoutubeLinks();
-
-// Make code blocks focusable, so they can be keyboard scrolled
-qsa('pre.astro-code').forEach((elem) => elem.setAttribute('tabindex', '0'));
-
-// Open external links in a new tab
-qsa('a[href^=http]').forEach((link) => {
-    // Open external links in a new tab
-    const destination = new URL(link.href);
-    if (destination.hostname != window.location.hostname) {
-        link.setAttribute('target', '_blank');
-    }
-});
+enhanceCodeBlocks();
---
+++ public/js/modules/code-blocks.js
@@ -0,0 +1,68 @@
+import { qs, qsa } from './query.js';
+
+const activeClass = "copy-button";
+
+const clipboard = `<svg xmlns="http://www.w3.org/2000/svg" title="Copy" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+<path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
+<rect x="9" y="3" width="6" height="4" rx="2" />
+</svg>`;
+
+const clipboardDone = `<svg xmlns="http://www.w3.org/2000/svg" title="Copied" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+<path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
+<rect x="9" y="3" width="6" height="4" rx="2" />
+<path d="M9 14l2 2l4 -4" />
+</svg>`;
+
+const clipboardError = `<svg xmlns="http://www.w3.org/2000/svg" title="Cannot copy" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+<path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
+<rect x="9" y="3" width="6" height="4" rx="2" />
+<path d="M10 12l4 4m0 -4l-4 4" />
+</svg>`
+
+/** 
+ * Enables copy on code blocks (<pre><code>...)
+*/
+function enhanceCodeBlocks() {
+
+    // Make code blocks focusable, so they can be keyboard scrolled
+    qsa('pre.astro-code').forEach((elem) => elem.setAttribute('tabindex', '0'));
+
+    // Open external links in a new tab
+    qsa('a[href^=http]').forEach((link) => {
+        // Open external links in a new tab
+        const destination = new URL(link.href);
+        if (destination.hostname != window.location.hostname) {
+            link.setAttribute('target', '_blank');
+        }
+    });
+
+    qsa(`pre:not(.${activeClass})`).forEach(node => {
+        const copy = document.createElement('button');
+        copy.classList.add(activeClass);
+        copy.innerHTML = clipboard;
+        copy.title = 'Copy';
+
+        const copyContainer = document.createElement('div');
+        copyContainer.className = 'copy-container';
+        copyContainer.appendChild(copy);
+
+        node.insertAdjacentElement('beforebegin', copyContainer);
+        copy.addEventListener('click', async () => {
+            if (navigator.clipboard) {
+                // @ts-ignore
+                const text = qs('code', node).innerText;
+                await navigator.clipboard.writeText(text);
+                copy.innerHTML = clipboardDone;
+            } else {
+                copy.innerHTML = clipboardError;
+            }
+
+            setTimeout(() => copy.innerHTML = clipboard, 2000);
+        });
+    });
+}
+
+export { enhanceCodeBlocks }
