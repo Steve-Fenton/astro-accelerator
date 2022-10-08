ef5b90a6c3d4b24ee0b8fb5220b3dda55f82935e
Steve Fenton <github@stevefenton.co.uk>
2022-10-08 22:22:01 +0100

    Tweaking animation base

--- public/js/modules/animation.js
+++ public/js/modules/animation.js
@@ -58,7 +58,7 @@ function addIntersectionObserver(listItemQuery) {
     const options = {
         root: null,
         rootMargin: '0px',
-        threshold: 0.5
+        threshold: 0.8
     };
 
     const observer = new IntersectionObserver(handleIntersection, options)
