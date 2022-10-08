cc17b1bb6a8820a2eff8a82148136f8ce937ec61
Steve Fenton <github@stevefenton.co.uk>
2022-10-08 21:24:21 +0100

    Mobile image animation capability

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ import { addResizedEvent } from './modules/resizing.js';
 import { setClickableBlocks } from './modules/click-blocks.js';
 import { addStickyNavigation } from './modules/nav-sticky.js';
 import { addMobileNavigation } from './modules/nav-mobile.js';
-import { addIntersectionObserver } from './modules/animation.js';
+import { addIntersectionObserver, addListImageIntersectionObserver } from './modules/animation.js';
 import { enhanceYoutubeLinks } from './modules/youtube.js';
 import { enhanceCodeBlocks } from './modules/code-blocks.js';
 
@@ -13,5 +13,6 @@ setClickableBlocks();
 addStickyNavigation('.site-header', '.site-nav', '.site-nav > ul', resizedEventName);
 addMobileNavigation('.navigation-icon', '.site-nav', resizedEventName);
 addIntersectionObserver('.post-list .list-item, main img, main .note, main blockquote');
+addListImageIntersectionObserver('.post-list .list-item img');
 enhanceYoutubeLinks();
 enhanceCodeBlocks();
--- public/js/modules/animation.js
+++ public/js/modules/animation.js
@@ -34,4 +34,36 @@ function addIntersectionObserver(listItemQuery) {
     qsa(listItemQuery).forEach((elem) => observer.observe(elem));
 }
 
-export { addIntersectionObserver };
+/**
+ * Assists animation by setting "--shown" CSS property
+ * 
+ * When an item is visible in the viewport, it will have --shown: 1
+ * Otherwise it will be --shown: 0
+ * This allows CSS transitions and calculated properties to animate elements
+ * 
+ * Example
+ *     transition: all 0.2s ease-in;
+ *     scale: calc(0.75 + (var(--shown, 1) * 0.25));
+ * 
+ * @param {string} imgItemQuery 
+ */
+ function addListImageIntersectionObserver(imgItemQuery) {
+    function handleIntersection(entries, observer) {
+        for (let entry of entries) {
+            const value = entry.isIntersecting ? 1 : 0;
+            entry.target.style.setProperty('--imgshown', value);
+        }
+    }
+
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5
+    };
+
+    const observer = new IntersectionObserver(handleIntersection, options)
+
+    qsa(imgItemQuery).forEach((elem) => observer.observe(elem));
+}
+
+export { addIntersectionObserver, addListImageIntersectionObserver };
