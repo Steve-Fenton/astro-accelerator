42adc0fe50d3b185f3900e3be56d8b468d83e09f
Steve Fenton <github@stevefenton.co.uk>
2022-11-25 12:41:16 +0000

    v0.0.30 config moved into utils

--- src/config.ts
+++ src/config.ts
@@ -1,64 +1,29 @@
-import type { Site } from 'astro-accelerator-utils';
+import { Config } from 'astro-accelerator-utils';
 
-export const SITE: Site = {
-	url: 'https://astro.stevefenton.co.uk',
-	subfolder: '',
-	feedUrl: '/articles/feed.xml',
-	title: 'Astro Accelerator',
-	description: 'An Astro quick start project.',
-	defaultLanguage: 'en',
-	themeColor: '#222255',
-	owner: 'Steve Fenton',
-	default: {
-		lang: 'en',
-		locale: 'en-GB',
-		dir: 'ltr'
-	},
-	search: {
-		fallbackUrl: 'https://www.google.com/search',
-		fallbackSite: 'q',
-		fallbackQuery: 'q',
-	},
-	pageSize: 2,
-	/*
-	Refers to "pages in the middle" of the automatically generated links:
-	- Prev
-	- First Page
-	- (A number of links, defined below)
-	- Last Page
-	- Next
-	*/
-	pageLinks: 3,
-	rssLimit: 20,
-	dateOptions: {
-		weekday: 'long',
-		year: 'numeric',
-		month: 'long',
-		day: 'numeric',
-	},
-	featureFlags: {
-		codeBlocks: ['copy'],
-		figures: ['enlarge'],
-		youTubeLinks: ['embed'],
-	},
-	images: {
-		contentSize: '(max-width: 860px) 100vw, 620px',
-		listerSize: '(max-width: 860px) 90vw, 350px',
-		authorSize: '50px',
-	}
-};
+const SITE = Config.getDefault();
+SITE.owner = 'Steve Fenton';
+SITE.url = 'https://astro.stevefenton.co.uk';
+SITE.feedUrl = '/articles/feed.xml';
+SITE.title= 'Astro Accelerator';
+SITE.description = 'An Astro quick start project.';
+SITE.themeColor = '#222255';
+SITE.defaultLanguage = 'en';
+SITE.default.locale = 'en-GB';
+SITE.pageSize = 2; // Allows testing of paging
 
 // Default image for OG: Tags
-export const OPEN_GRAPH = {
+const OPEN_GRAPH = {
 	image: {
 		src: '/img/surface-accessories.png',
 		alt: 'Alt text for image goes here',
 	}
 };
 
-export const HEADER_SCRIPTS = `
+const HEADER_SCRIPTS = `
 <!-- HEADER SCRIPTS -->
 `.trim();
+
+export { SITE, OPEN_GRAPH, HEADER_SCRIPTS }
 
 export type Frontmatter = {
 	layout: string;
