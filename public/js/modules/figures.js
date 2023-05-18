7dc48fe70c92fddfd45e33f43cc4783838011f44
Steve Fenton <99181436+steve-fenton-octopus@users.noreply.github.com>
2023-05-18 10:16:44 +0100

    Move enlarge icon into CSS

--- public/js/modules/figures.js
+++ public/js/modules/figures.js
@@ -2,16 +2,8 @@ import { qs, qsa } from './query.js';
 
 const activeClass = 'magnify-icon';
 
-const clipboard = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <line x1="15" y1="8" x2="15.01" y2="8" />
-  <rect x="4" y="4" width="16" height="16" rx="3" />
-  <path d="M4 15l4 -4a3 5 0 0 1 3 0l5 5" />
-  <path d="M14 14l1 -1a3 5 0 0 1 3 0l2 2" />
-</svg>`;
-
 /** 
- * Enables copy on code blocks (<pre><code>...)
+ * Enables opening image in new tab
 */
 function enhanceFigures() {
     qsa(`figure > p > img`).forEach(node => {
@@ -19,7 +11,6 @@ function enhanceFigures() {
 
         const magnify = document.createElement('button');
         magnify.classList.add(activeClass);
-        magnify.innerHTML = clipboard;
         magnify.title = 'Enlarge';
 
         const magnifyContainer = document.createElement('div');
