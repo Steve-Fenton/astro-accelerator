df330fd86b53049374b086337f3625a5a00ac5e3
Steve Fenton <Steve-Fenton@users.noreply.github.com>
2024-07-29 08:19:10 +0000

    Update dependencies

--- public/js/modules/search-dialog.js
+++ public/js/modules/search-dialog.js
@@ -28,7 +28,7 @@ function enhanceSearchIcon() {
                     close.id = 'site-search-close';
                     close.value = 'cancel';
                     close.formMethod = 'dialog';
-                    close.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    close.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" title="Close" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                     <line x1="18" y1="6" x2="6" y2="18"></line>
                     <line x1="6" y1="6" x2="18" y2="18"></line>
