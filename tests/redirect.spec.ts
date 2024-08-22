edad8a997d17e93ead5b27b6802afe069abe3f75
Steve Fenton <Steve-Fenton@users.noreply.github.com>
2024-08-22 14:38:44 +0000

    Latest incarnation of the redirect test

--- tests/redirect.spec.ts
+++ tests/redirect.spec.ts
@@ -1,5 +1,3 @@
-/** @format */
-
 import { test, expect } from '@playwright/test';
 import { glob } from 'glob';
 import fs from 'fs';
@@ -7,60 +5,50 @@ import fs from 'fs';
 const baseUrl = 'http://[::1]:3000';
 const regex = /<meta http-equiv="refresh" content="0; URL=(.*?)">/;
 const redirectMatcher = /redirect:\s?(.*?)\r?\n/;
-const posts = await glob('src/pages/docs/**/*.{md,mdx}');
+const posts = await glob('src/pages/**/*.{md,mdx}');
 
 test('Check redirects', async () => {
-    for (let post of posts) {
-        let url = null;
-        const content = fs.readFileSync(post).toString();
-        const redirectMatches = content.match(redirectMatcher);
+  let count = 0;
 
-        if (redirectMatches) {
-            url = baseUrl + redirectMatches[1];
-        }
+  for (let post of posts) {
+    let url = null;
+    const content = fs.readFileSync(post).toString();
+    const redirectMatches = content.match(redirectMatcher);
 
-        try {
-            if (url && url.startsWith('/')) {
-                // Get the redirect page
-                const redirectPage = await fetch(new URL(url));
-                expect(
-                    redirectPage.status,
-                    `Expected a 200 OK response for page ${url}`
-                ).toBe(200);
-
-                // Get the new location
-                const redirectText = await redirectPage.text();
-                const matches = redirectText.match(regex);
-
-                if (
-                    matches &&
-                    matches.length > 1 &&
-                    matches[1].charAt(0) === '/'
-                ) {
-                    // Check the new location works
-                    const targetPage = await fetch(
-                        new URL(baseUrl + matches[1])
-                    );
-                    expect(
-                        targetPage.status,
-                        `Expected a 200 OK response for target page ${matches[1]}`
-                    ).toBe(200);
-
-                    // Make sure the new location isn't a redirect page
-                    const targetText = await targetPage.text();
-                    const targetMatches = targetText.match(regex);
-
-                    if (targetMatches && targetMatches.length > 0) {
-                        expect(
-                            `Target page ${matches[1]} contains a redirect`
-                        ).toBe('');
-                    }
-                }
-            }
-        } catch (error) {
-            expect(
-                `Failed to fetch redirect target ${url} found on ${post} due to ${error}`
-            ).toBe('');
-        }
+    if (redirectMatches) {
+      url = redirectMatches[1];
     }
+
+    try {
+      if (url && url.startsWith('/')) {
+        count++;
+
+        // Get the redirect page
+        const redirectPage = await fetch(new URL(baseUrl + url));
+        expect(redirectPage.status, `Expected a 200 OK response for page ${url}`).toBe(200);
+
+        // Get the new location
+        const redirectText = await redirectPage.text();
+        const matches = redirectText.match(regex);
+
+        if (matches && matches.length > 1 && matches[1].charAt(0) === '/') {
+          // Check the new location works
+          const targetPage = await fetch(new URL(baseUrl + matches[1]))
+          expect(targetPage.status, `Expected a 200 OK response for target page ${matches[1]}`).toBe(200);
+          
+          // Make sure the new location isn't a redirect page
+          const targetText = await targetPage.text();
+          const targetMatches = targetText.match(regex);
+        
+          if (targetMatches && targetMatches.length > 0) {
+            expect(`Target page ${matches[1]} contains a redirect`).toBe('');
+          }
+        }
+      }
+    } catch(error) {
+      expect(`Failed to fetch redirect target ${url} found on ${post} due to ${error}`).toBe('');
+    }
+  }
+
+  console.log(`Checked ${count} redirects`);
 });
