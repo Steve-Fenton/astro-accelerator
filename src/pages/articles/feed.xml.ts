050ec98a9e096d5f31dfaca10a1bf217cf4baac4
Steve Fenton <Steve-Fenton@users.noreply.github.com>
2024-09-27 14:13:33 +0000

    Update getTextFrom usage

--- src/pages/articles/feed.xml.ts
+++ src/pages/articles/feed.xml.ts
@@ -1,3 +1,5 @@
+/** @format */
+
 // warning: This file is overwritten by Astro Accelerator
 
 // Generates an ATOM feed of recent posts
@@ -5,60 +7,67 @@ import { SITE } from '@config';
 import { Accelerator, PostFiltering } from 'astro-accelerator-utils';
 
 async function getData() {
-  //@ts-ignore
-  const allArticles = import.meta.glob(['./**/*.md', './**/*.mdx']);
+    //@ts-ignore
+    const allArticles = import.meta.glob(['./**/*.md', './**/*.mdx']);
 
-  const accelerator = new Accelerator(SITE);
-  const stats = new accelerator.statistics('pages/articles/feed.xml');
-  stats.start();
-  
-  let articles = [];
-  
-  for (const path in allArticles) {
-    const article: any = await allArticles[path]();
+    const accelerator = new Accelerator(SITE);
+    const stats = new accelerator.statistics('pages/articles/feed.xml');
+    stats.start();
 
-    if (PostFiltering.isListable(article)) {
-      article.frontmatter.title = await accelerator.markdown.getTextFrom(article.frontmatter.title ?? '');
+    let articles = [];
 
-      articles.push({
-        url: article.url,
-        frontmatter: article.frontmatter
-      });
+    for (const path in allArticles) {
+        const article: any = await allArticles[path]();
+
+        if (PostFiltering.isListable(article)) {
+            article.frontmatter.title = await accelerator.markdown.getTextFrom(
+                article.frontmatter?.title
+            );
+
+            articles.push({
+                url: article.url,
+                frontmatter: article.frontmatter,
+            });
+        }
     }
-  }
 
-  articles =  articles.sort((a, b) => b.frontmatter.pubDate.localeCompare(a.frontmatter.pubDate));
+    articles = articles.sort((a, b) =>
+        b.frontmatter.pubDate.localeCompare(a.frontmatter.pubDate)
+    );
 
-  const limit = SITE.rssLimit ?? 20;
-  const items = articles
-    .slice(0, limit)
-    .map(a => `
+    const limit = SITE.rssLimit ?? 20;
+    const items = articles.slice(0, limit).map(
+        (a) => `
     <entry>
       <title>${a.frontmatter.title ?? ''}</title>
-      <link href="${ SITE.url + a.url }" />
-      <id>${ SITE.url + accelerator.urlFormatter.formatAddress(a.url) }</id>
-      <published>${ a.frontmatter.pubDate }</published>
-      <updated>${ a.frontmatter.pubDate ?? a.frontmatter.pubDate }</updated>
-      <summary>${ a.frontmatter.description ?? '' }</summary>
-    </entry>`);
+      <link href="${SITE.url + a.url}" />
+      <id>${SITE.url + accelerator.urlFormatter.formatAddress(a.url)}</id>
+      <published>${a.frontmatter.pubDate}</published>
+      <updated>${a.frontmatter.pubDate ?? a.frontmatter.pubDate}</updated>
+      <summary>${a.frontmatter.description ?? ''}</summary>
+    </entry>`
+    );
 
-  stats.stop();
-  
-  return new Response(`<?xml version="1.0" encoding="utf-8"?>
+    stats.stop();
+
+    return new Response(
+        `<?xml version="1.0" encoding="utf-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
-  <title>${ SITE.title }</title>
-  <subtitle>${ SITE.description }</subtitle>
-  <link href="${ SITE.url }/atom.xml" rel="self" />
-  <link href="${ SITE.url }" />
-  <id>${ SITE.url }/atom.xml</id>
-  <updated>${  articles[0].frontmatter.pubDate }</updated>
+  <title>${SITE.title}</title>
+  <subtitle>${SITE.description}</subtitle>
+  <link href="${SITE.url}/atom.xml" rel="self" />
+  <link href="${SITE.url}" />
+  <id>${SITE.url}/atom.xml</id>
+  <updated>${articles[0].frontmatter.pubDate}</updated>
 ${items.join('')}
-</feed>`, {
-    status: 200,
-    headers: {
-    'Content-Type': "application/xml"
-    }
-  });
+</feed>`,
+        {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/xml',
+            },
+        }
+    );
 }
 
 export const GET = getData;
--- src/pages/search.json.ts
+++ src/pages/search.json.ts
@@ -1,3 +1,5 @@
+/** @format */
+
 // warning: This file is overwritten by Astro Accelerator
 
 import { Accelerator, PostFiltering } from 'astro-accelerator-utils';
@@ -5,7 +7,6 @@ import type { MarkdownInstance } from 'astro';
 import { SITE } from '@config';
 import { htmlToText, convert } from 'html-to-text';
 import keywordExtractor from 'keyword-extractor';
-
 
 const getData = async () => {
     //@ts-ignore
@@ -15,22 +16,26 @@ const getData = async () => {
     const accelerator = new Accelerator(SITE);
 
     for (const path in allPages) {
-        const page = await allPages[path]() as  MarkdownInstance<Record<string, any>>;
+        const page = (await allPages[path]()) as MarkdownInstance<
+            Record<string, any>
+        >;
 
         if (!PostFiltering.showInSearch(page)) {
             continue;
         }
 
         let url = page.url ?? '';
-    
+
         if (page.frontmatter.paged) {
             url += '/1/';
         }
 
         const headings = await page.getHeadings();
-        const title = await accelerator.markdown.getTextFrom(page.frontmatter.title ?? '');
+        const title = await accelerator.markdown.getTextFrom(
+            page.frontmatter?.title
+        );
         const content = page.compiledContent ? page.compiledContent() : '';
-        let counted: { word: string, count: number }[] = [];
+        let counted: { word: string; count: number }[] = [];
 
         if (content) {
             const text = convert(content, { wordwrap: false });
@@ -38,33 +43,38 @@ const getData = async () => {
             const words = keywordExtractor.extract(text, {
                 language: 'english',
                 return_changed_case: true,
-                remove_duplicates: true
+                remove_duplicates: true,
             });
 
-            counted = words.map((w) => {
-                return { word: w, count: words.filter(wd => wd === w).length };
-            }).filter(e => e.word.replace(/[^a-z]+/g, '').length > 1);
+            counted = words
+                .map((w) => {
+                    return {
+                        word: w,
+                        count: words.filter((wd) => wd === w).length,
+                    };
+                })
+                .filter((e) => e.word.replace(/[^a-z]+/g, '').length > 1);
         }
 
         items.push({
             title: title,
-            headings: headings.map(h => {
-                return {text: h.text, slug: h.slug }
+            headings: headings.map((h) => {
+                return { text: h.text, slug: h.slug };
             }),
             description: page.frontmatter.description ?? '',
-            keywords: counted.map(c => c.word).join(' '),
+            keywords: counted.map((c) => c.word).join(' '),
             tags: page.frontmatter.tags ?? [],
             url: SITE.url + accelerator.urlFormatter.formatAddress(url),
-            date: page.frontmatter.pubDate ?? ''
+            date: page.frontmatter.pubDate ?? '',
         });
     }
 
     return new Response(JSON.stringify(items), {
         status: 200,
         headers: {
-        'Content-Type': "application/json"
-        }
+            'Content-Type': 'application/json',
+        },
     });
-}
+};
 
 export const GET = getData;
