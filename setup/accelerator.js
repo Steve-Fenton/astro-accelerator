f505bf7a6736131248104fe8e8082a489250fc10
Steve Fenton <github@stevefenton.co.uk>
2025-09-06 16:59:27 +0100

    Style adjustments

--- setup/accelerator.js
+++ setup/accelerator.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import fs from 'fs';
 import path from 'path';
 
@@ -10,63 +12,45 @@ fs.mkdirSync(path.join(workingDirectory, '/src/layouts'), { recursive: true });
 fs.mkdirSync(path.join(workingDirectory, '/src/pages'), { recursive: true });
 
 // Always Update
+copyRecursiveSync('./node_modules/astro-accelerator/src/themes/', './src/themes/', true);
+
+copyRecursiveSync('./node_modules/astro-accelerator/public/css/', './public/css/', true);
+
+copyRecursiveSync('./node_modules/astro-accelerator/src/pages/search.json.ts', './src/pages/search.json.ts', true);
+
+copyRecursiveSync('./node_modules/astro-accelerator/src/pages/sitemap.xml.ts', './src/pages/sitemap.xml.ts', true);
+
 copyRecursiveSync(
-  './node_modules/astro-accelerator/src/themes/',
-  './src/themes/',
-  true
+    './node_modules/astro-accelerator/src/pages/articles/',
+    // You can change the destination below from "articles" to something else, like blog
+    './src/pages/articles/',
+    true
 );
 
 copyRecursiveSync(
-  './node_modules/astro-accelerator/src/pages/search.json.ts',
-  './src/pages/search.json.ts',
-  true
+    './node_modules/astro-accelerator/src/pages/authors/',
+    // You can change the destination below from "articles" to something else, like blog
+    './src/pages/authors/',
+    true
 );
 
 copyRecursiveSync(
-  './node_modules/astro-accelerator/src/pages/sitemap.xml.ts',
-  './src/pages/sitemap.xml.ts',
-  true
+    './node_modules/astro-accelerator/src/pages/category/',
+    // You can change the destination below from "category" to something else, like interest
+    './src/pages/category/',
+    true
 );
 
 copyRecursiveSync(
-  './node_modules/astro-accelerator/src/pages/articles/',
-  // You can change the destination below from "articles" to something else, like blog
-  './src/pages/articles/',
-  true
+    './node_modules/astro-accelerator/src/pages/tag/',
+    // You can change the destination below from "tag" to something else, like label
+    './src/pages/tag/',
+    true
 );
 
-copyRecursiveSync(
-  './node_modules/astro-accelerator/src/pages/authors/',
-  // You can change the destination below from "articles" to something else, like blog
-  './src/pages/authors/',
-  true
-);
+copyRecursiveSync('./node_modules/astro-accelerator/src/layouts/', './src/layouts/', true);
 
-copyRecursiveSync(
-  './node_modules/astro-accelerator/src/pages/category/',
-  // You can change the destination below from "category" to something else, like interest
-  './src/pages/category/',
-  true
-);
-
-copyRecursiveSync(
-  './node_modules/astro-accelerator/src/pages/tag/',
-  // You can change the destination below from "tag" to something else, like label
-  './src/pages/tag/',
-  true
-);
-
-copyRecursiveSync(
-  './node_modules/astro-accelerator/src/layouts/',
-  './src/layouts/',
-  true
-);
-
-copyRecursiveSync(
-  './node_modules/astro-accelerator/src/pages/search.md',
-  './src/pages/search.md',
-  true
-);
+copyRecursiveSync('./node_modules/astro-accelerator/src/pages/search.md', './src/pages/search.md', true);
 
 // More cautious updates (Usually only on first setup)
 copyRecursiveSync('./node_modules/astro-accelerator/sample/src/pages/', './src/pages/', forcedUpdate);
@@ -84,20 +68,20 @@ copyRecursiveSync('./node_modules/astro-accelerator/tsconfig.json', './tsconfig.
  * @param {string} dest The path to the new copy.
  */
 function copyRecursiveSync(src, dest, overwrite) {
-  const exists = fs.existsSync(src);
-  const stats = exists && fs.statSync(src);
-  const isDirectory = exists && stats.isDirectory();
+    const exists = fs.existsSync(src);
+    const stats = exists && fs.statSync(src);
+    const isDirectory = exists && stats.isDirectory();
 
-  if (isDirectory) {
-    fs.mkdirSync(dest, { recursive: true });
-    fs.readdirSync(src).forEach((childItemName) => {
-      copyRecursiveSync(path.join(src, childItemName), path.join(dest, childItemName), overwrite);
-    });
-  } else {
-    if (overwrite || (fs.existsSync(dest) == false)) {
-      fs.copyFileSync(src, dest);
+    if (isDirectory) {
+        fs.mkdirSync(dest, { recursive: true });
+        fs.readdirSync(src).forEach(childItemName => {
+            copyRecursiveSync(path.join(src, childItemName), path.join(dest, childItemName), overwrite);
+        });
     } else {
-      console.log('Skipping', dest);
+        if (overwrite || fs.existsSync(dest) == false) {
+            fs.copyFileSync(src, dest);
+        } else {
+            console.log('Skipping', dest);
+        }
     }
-  }
 }
