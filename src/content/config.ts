c22131215c80d89f9c2115fcfb5c741581f636be
Steve Fenton <github@stevefenton.co.uk>
2026-02-16 13:36:07 +0000

    Migrate to Content Layers API

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,8 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const events = defineCollection({
+    loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/events" }),
     schema: z.object({
         title: z.string(),
         description: z.string(),
